757380fccbbbc25e7f91615333ea8d85dd539025
Haridugrani <bhargavdungrani32@gmail.com>
2025-04-04 16:19:40 +0530

    update

--- index.js
+++ index.js
@@ -39,7 +39,7 @@ app.get("/", (req, res) => {
 app.get("/api/testing", (req, res) => {
 	return res.json({
 		success:true,
-		message:'Your server is up and running and this is bhagu...'
+		message:'Your server is up and running and this is bhagughhhfghdh...'
 	});
 });
 
